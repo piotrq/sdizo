b614503343af4b7d6f2c2efee4f2b43d46ad547f
Piotr Mackowiak <martoip123@gmail.com>
2012-03-26 13:52:09 +0200

    Testowy commit + założony projekt w visual Studio 2010

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <conio.h>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+	char a[100];
+	cout<<"Podaj swe zacne imie :) : ";
+	cin>>a;
+	cout<<endl<<"Witaj "<<a<<endl;
+	getch();
+	return 0;
+
+
+}
